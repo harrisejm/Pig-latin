3dc5b92322749e78a72ff3e730642165527e2d27
Brian Palowski and Eddie Harris <brian@>
2018-06-06 14:12:50 -0700

    https://github.com/harrisejm/Pig-latin

--- js/scripts.js
+++ js/scripts.js
@@ -7,37 +7,42 @@ $(document).ready(function() {
     var arr = string1.split(" ");
     var arr1 = [];
 
-
     for ( var i=0; i<arr.length; i++){
       var newArr = arr[i];
       var newArr2 = newArr.split("");
       arr1.push(newArr2);
     }
-
-    //$("#result").text(arr1[0][1]);
-
-    var vowels = ["a", "e", "i", "o", "u"];
     var test = ""
 
+    for (var a = 0; a < arr1.length; a++) {
+      var outPut = arr1[a];
+      var outPut2 = outPut.join("");
 
-    for (var a = 0; a < arr.length; a++) { //loop through array
-      for (var i = 0; i < vowels.length; i++) { //loop through vowels
-        if (arr1[a][0] === vowels[i]) {
-          var outPut = arr1[a];
-          var outPut2 = outPut.join("");
-          test += outPut2 + "Way ";
-        // test += arr1[a].length;
-      } else if (arr1[0][0] !== vowels[0]) {
-      //
-      //
-             test += "TEST";
-       }
-          }
-          }
-  //  $("#result").text(arr1[0] + "Way");
-  $("#result").text(test);
+      if (arr1[a][0] === "a" || arr1[a][0] === "e" || arr1[a][0] === "i" || arr1[a][0] === "o" || arr1[a][0] === "u") {
+        test += outPut2 + "Way ";
 
-//arr1[a].length &&
+      } else if (arr1[a][0] === "q" && arr1[a][1] === "u") {
+        var singleConsonant = arr1[a].slice(2);
+        singleConsonant.splice(arr1[a].length, 0, arr[a].slice(0,2));
+        var singleConsonantOutput = singleConsonant.join("");
+        test += singleConsonantOutput + " ";
+
+      } else if ((arr1[a][0] !== "a" || arr1[a][0] !== "e" || arr1[a][0] !== "i" || arr1[a][0] !== "o" || arr1[a][0] !== "u")
+      && (arr1[a][1] !== "a" || arr1[a][1] !== "e" || arr1[a][1] !== "i" || arr1[a][1] !== "o" || arr1[a][1] !== "u")) {
+
+        var singleConsonant = arr1[a].slice(2);
+        singleConsonant.splice(arr1[a].length, 0, arr[a].slice(0,2));
+        var singleConsonantOutput = singleConsonant.join("");
+        test += singleConsonantOutput + "Ay" + " ";
+      } else if (arr1[a][0] !== "a" || arr1[a][0] !== "e" || arr1[a][0] !== "i" || arr1[a][0] !== "o" || arr1[a][0] !== "u") {
+        var singleConsonant = arr1[a].slice(1);
+        singleConsonant.splice(arr1[a].length, 0, arr[a].slice(0,1));
+        var singleConsonantOutput = singleConsonant.join("");
+        test += singleConsonantOutput + "Ay" + " ";
+      }
+    }
+
+    $("#result").text(test);
 
   });
 });
